310110e8646ed1bbc8d8d69668513424d6f8eebf
jamount <55700813+jamount@users.noreply.github.com>
2020-01-06 12:49:51 +0000

    added waves but not working
    the lizards do spawn but the sprites dont appear and dont move

---
+++ Assets/Scripts/wavesystem.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wavesystem : MonoBehaviour
+{
+    public float adjustmentAngle = 0;
+    public enum SpawnState { SPAWNING, WAITING, COUNTING};
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy1;
+        public int count1;
+        public float rate1;
+
+        public Transform enemy2;
+        public int count2;
+        public float rate2;
+
+        public Transform enemy3;
+        public int count3;
+        public float rate3;
+
+        public Transform enemy4;
+        public int count4;
+        public float rate4;
+
+        public Transform enemy5;
+        public int count5;
+        public float rate5;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public Transform[] spawnPoints;
+
+    public float timeBetweenWaves = 5f;
+    public float waveCountdown;
+
+    private float searchCountdown = 1f;
+
+    public SpawnState state = SpawnState.COUNTING;
+
+    private void Start()
+    {
+        waveCountdown = timeBetweenWaves;
+
+    }
+
+    private void Update()
+    {
+        if (state == SpawnState.WAITING)
+        {
+            if (!EnemyIsAlive())
+            {
+                WaveCompleted();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        if(waveCountdown <= 0)
+        {
+            if (state != SpawnState.SPAWNING)
+            {
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
+        
+        }
+        else
+        {
+            waveCountdown -= Time.deltaTime;
+        }
+    }
+
+    void WaveCompleted()
+    {
+        Debug.Log("Wave Completed!");
+
+        state = SpawnState.COUNTING;
+        waveCountdown = timeBetweenWaves;
+
+        if (nextWave + 1 > waves.Length - 1)
+        {
+            nextWave = 0;
+            Debug.Log("ALL WAVES COMPLETE! back to wave 1");
+        }
+        else
+        {
+            nextWave++;
+        }
+        
+    }
+
+
+    bool EnemyIsAlive()
+    {
+        searchCountdown -= Time.deltaTime;
+        if (searchCountdown <= 0f)
+        {
+            searchCountdown = 1f;
+            if (GameObject.FindGameObjectWithTag("Enemy") == null)
+            {
+                return false;
+            }
+        }
+            return true;
+        
+    }
+
+
+    IEnumerator SpawnWave(Wave _wave)
+    {
+        Debug.Log("Spawning Wave: " + _wave.name);
+        state = SpawnState.SPAWNING;
+
+        for (int i = 0; i < _wave.count1; i++)
+        {
+            SpawnEnemy(_wave.enemy1);
+            yield return new WaitForSeconds(1f / _wave.rate1);
+
+        }
+
+        for (int i = 0; i < _wave.count2; i++)
+        {
+            SpawnEnemy(_wave.enemy2);
+            yield return new WaitForSeconds(1f / _wave.rate2);
+        }
+
+        for (int i = 0; i < _wave.count3; i++)
+        {
+            SpawnEnemy(_wave.enemy3);
+            yield return new WaitForSeconds(1f / _wave.rate3);
+        }
+
+        for (int i = 0; i < _wave.count4; i++)
+        {
+            SpawnEnemy(_wave.enemy4);
+            yield return new WaitForSeconds(1f / _wave.rate4);
+        }
+
+        for (int i = 0; i < _wave.count5; i++)
+        {
+            SpawnEnemy(_wave.enemy5);
+            yield return new WaitForSeconds(1f / _wave.rate5);
+        }
+
+        state = SpawnState.WAITING;
+        yield break;
+    }
+
+    void SpawnEnemy(Transform _enemy)
+    {
+        Debug.Log("Spawning Enemy:" + _enemy.name);
+        Transform _sp = spawnPoints[Random.Range(0,spawnPoints.Length)];
+
+        
+        
+        Instantiate(_enemy, _sp.position, _sp.rotation);
+    }
+}
